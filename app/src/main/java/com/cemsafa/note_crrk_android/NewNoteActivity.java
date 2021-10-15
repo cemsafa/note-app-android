bd7dd7743b95f21c9fe8c42b3882e63be4f28761
- <C0814038@mylambton.ca>
2021-10-15 21:13:01 +0530

    Worked on Image callback

--- app/src/main/java/com/cemsafa/note_crrk_android/NewNoteActivity.java
+++ app/src/main/java/com/cemsafa/note_crrk_android/NewNoteActivity.java
@@ -6,9 +6,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class NewNoteActivity extends AppCompatActivity {
 
@@ -65,5 +70,33 @@ public class NewNoteActivity extends AppCompatActivity {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("image/*");
         startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (reqCode == CAMERA_REQUEST && resultCode == RESULT_OK)
+        {
+            image = (Bitmap) data.getExtras().get("data");
+
+        }
+
+        else if(reqCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+            Uri uri = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        else{
+
+        }
     }
 }
