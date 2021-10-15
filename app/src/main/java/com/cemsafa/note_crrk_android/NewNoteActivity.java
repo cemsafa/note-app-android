118a31f0040aa2db47290a186d2132c4b3e35822
- <C0814038@mylambton.ca>
2021-10-15 21:04:46 +0530

    Adding view binding

--- app/src/main/java/com/cemsafa/note_crrk_android/NewNoteActivity.java
+++ app/src/main/java/com/cemsafa/note_crrk_android/NewNoteActivity.java
@@ -3,12 +3,27 @@ package com.cemsafa.note_crrk_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewNoteActivity extends AppCompatActivity {
+
+    Button select;
+    private static final int CAMERA_REQUEST = 102;
+    private static final int GALLERY_REQUEST = 101;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+        select=(Button) findViewById(R.id.select);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
